c47758a886799c3d2a10dca353c0469c1fc503d3
frudotz <46935560+frudotz@users.noreply.github.com>
2023-04-28 19:03:32 +0300

    Update socket.js

--- socket.js
+++ socket.js
@@ -10,10 +10,8 @@ const elements = {
     discriminator: document.getElementById("tag"),
     avatar: document.getElementById("avatar"),
     status: document.getElementById("status"),
-    status2: document.getElementById("status2"),
     card: document.getElementById("profile"),
     durum: document.getElementById("durum"),
-    durumm: document.getElementById("spodurum"),
     hangicihaz: document.getElementById("aktiflikyeri"),
 };
 
@@ -29,7 +27,7 @@ lanyard.onmessage = ({ data }) => {
             JSON.stringify({
                 op: OPCODES.INIT,
                 d: {
-                    subscribe_to_id: "460867393141342218",
+                    subscribe_to_id: "701896585604497490",
                 },
             })
         );
@@ -52,7 +50,7 @@ lanyard.onmessage = ({ data }) => {
 
         if (parsedData.t == "INIT_STATE") {
             const user = parsedData.d;
-            elements.avatar.src = `https://cdn.discordapp.com/avatars/460867393141342218/${user.discord_user.avatar}?size=128`;
+            elements.avatar.src = `https://cdn.discordapp.com/avatars/701896585604497490/${user.discord_user.avatar}?size=128`;
             var a = 0
             console.log(user)
             elements.card.style.opacity = "1";
@@ -65,17 +63,16 @@ lanyard.onmessage = ({ data }) => {
             if (!user.activities[0]){
             a += 1
             } if (a == 3){
-            elements.durum.innerText ="Nothing"
+            elements.durum.innerText ="Discord Status: Anything"
             } else if ( a == 2) {
-            elements.durum.innerText =user.activities[0].state
+            elements.durum.innerText ="Discord Status: "+ user.activities[0].state
             } else if (a == 0){
-            elements.durum.innerText =user.activities[0].state
-            elements.durumm.innerText =user.activities[1].name
+            elements.durum.innerText ="Discord Status: "+ user.activities[0].state+"\n What am I doing: "+user.activities[1].name
             
             }
             if(user.listening_to_spotify== true){
-                document.getElementById("spotify-artist").innerText = user.spotify.artist
-                document.getElementById("spotify-song").innerText = user.spotify.song
+                document.getElementById("spotify-artist").innerText = "\n"+ user.spotify.artist
+                document.getElementById("spotify-song").innerText = "\n"+ user.spotify.song
                 document.getElementById("spotify-pic").src = user.spotify.album_art_url
                 document.getElementById("spotify-pic").style.visibility = "visible";
             }
@@ -90,15 +87,11 @@ lanyard.onmessage = ({ data }) => {
             }
             elements.status.style.background =
                 statusColors[user.discord_status];
-          elements.status2.style.background =
-                statusColors[user.discord_status];
             
             
         } else if (parsedData.t == "PRESENCE_UPDATE") {
             const user = parsedData.d;
             elements.status.style.background =
-                statusColors[user.discord_status];
-          elements.status2.style.background =
                 statusColors[user.discord_status];
         }
     }
